325222608985757c986a4e4443ac6978f10c2443
adelbouras <eyyad.kareem1212@gmail.com>
2025-09-14 16:55:15 +0100

    more colorscheme added

--- lua/plugins/setups/colorscheme.lua
+++ lua/plugins/setups/colorscheme.lua
@@ -1,20 +1,32 @@
 local function transparent()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 return {
-    --    {"ellisonleao/gruvbox.nvim",
-    --    priority = 1000 ,
-    --    config = function()
-    --        vim.cmd.colorscheme "gruvbox"
-    --        vim.o.background = "dark"
-    --       -- transparent() uncomment for transparent nvim bg
-    --    end
-    --    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "gruvbox"
+            vim.o.background = "dark"
+            transparent() --uncomment for transparent nvim bg
+        end
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = true,
+        config = function()
+            vim.cmd.colorscheme "catppuccin"
+            transparent() --uncomment for transparent nvim bg
+        end
+    },
     {
         "folke/tokyonight.nvim",
+        lazy = true,
         config = function()
             vim.cmd.colorscheme "tokyonight"
-            -- transparent() --uncomment for transparent nvim bg
+            transparent() --uncomment for transparent nvim bg
         end
-    }
+    },
 }
