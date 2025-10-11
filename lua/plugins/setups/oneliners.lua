7ecf34241b1f6de795a8a9e8e873b2b88e99b741
adelbouras <eyyad.kareem1212@gmail.com>
2025-10-11 19:49:09 +0100

    add: Dbee for sql simplisity ui

--- lua/plugins/setups/oneliners.lua
+++ lua/plugins/setups/oneliners.lua
@@ -1,15 +1,13 @@
 return {
     {
-        {
-            "kndndrj/nvim-dbee",
-            dependencies = { "MunifTanjim/nui.nvim" },
-            build = function()
-                require("dbee").install()
-            end,
-            config = function()
-                require("dbee").setup()
-            end,
-        }
+        "kndndrj/nvim-dbee",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        build = function()
+            require("dbee").install()
+        end,
+        config = function()
+            require("dbee").setup()
+        end,
     },
     { "tpope/vim-fugitive" },
     {
