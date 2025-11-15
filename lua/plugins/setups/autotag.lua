857f255c3ef04ecc6f586ee539759af910d8b8d6
adelbouras <eyyad.kareem1212@gmail.com>
2025-11-15 03:14:05 +0100

    add: highlight the closed tag when hovering on open tag

--- lua/plugins/setups/autotag.lua
+++ lua/plugins/setups/autotag.lua
@@ -3,8 +3,19 @@ return {
         "windwp/nvim-ts-autotag",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         config = function()
-            require("nvim-ts-autotag").setup()
+            require("nvim-treesitter.configs").setup({
+                matchup = {
+                    enable = true,
+                    disable_virtual_text = true,
+                },
+            })
         end,
-    }
-
+    },
+    {
+        "andymass/vim-matchup",
+        config = function()
+            vim.g.matchup_matchparen_offscreen = { method = "popup" }
+            vim.g.matchup_matchparen_deferred = 1
+        end,
+    },
 }
